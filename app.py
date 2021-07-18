e46fbf682c9022f3dfe97692be5260657752c479
Ram Haridas <ramharidas31@gmail.com>
2021-07-18 18:56:08 +0530

    app

--- app.py
+++ app.py
@@ -49,9 +49,9 @@ def home():
 
 
 #keep this commented out for online
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 
